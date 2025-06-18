06f1a0ec86dbed8d0be983f7916da6856e2118c0
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-06-18 22:50:17 +0000

    completed

--- src/app.py
+++ src/app.py
@@ -6,16 +6,14 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-# from models import Person
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# Create the jackson family object
+# Create the jackson family object 
 jackson_family = FamilyStructure("Jackson")
-
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -28,16 +26,43 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-
+# get all 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
+
+# get individual
+@app.route('/members/<int:id>', methods=['GET'])
+def get_each_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
 
 
+@app.route('/members', methods=['POST'])
+def add_new_member():
+    member_data = request.get_json()
+    if not member_data:
+        return jsonify({"error": "Missing JSON body"}), 400
+
+    jackson_family.add_member(member_data)
+
+    
+    return jsonify(member_data), 200
+
+
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member = jackson_family.get_member(id)
+    if not member:
+        return jsonify({"error": "Member not found"}), 404
+
+    jackson_family.delete_member(id)
+    return jsonify({"done": True}), 200
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
